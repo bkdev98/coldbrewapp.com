e3a7c89c06886fcb786b54cefbd42eb703f0658e
Quốc Khánh <bkdev98@gmail.com>
2019-05-06 17:07:48 +0700

    initials

---
+++ site-config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  // App Info
+  appstore_link         : "https://coldbrewapp.com",                                  // Enter App Store URL.
+  playstore_link        : null,                                  // Enter Google Play Store URL.
+  google_analytics_ID   : "UA-47311644-5",                                        // Enter Google Analytics ID or ""
+  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot   : "video",                                           // "screenshot" or "video"
+  app_url               : "https://coldbrewapp.com",                            // Domain of your website without path_prefix.
+  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name              : "COLD BREW",
+  app_price             : "Free",
+  app_description       : "Split bill, share with your friends and have fun.",
+  app_keywords          : ["cold brew", "split bill", "manage invoice", "coldbrewapp"],
+
+  // Personal Info
+  your_name              : "Quoc Khanh",
+  your_link              : "https://qckhnh.com",
+  your_city              : "Sai Gon",
+  email_address          : "khanhbq@innoteq.vn",
+  facebook_username      : "bkdev98",
+  instagram_username     : "qckhnh",
+  twitter_username       : "bkdev98",
+  github_username        : "bkdev98",
+  youtube_username       : null,
+
+  // Features List
+  features               : [
+    {
+      title: "Scan bill",
+      description:
+        "Scan and process bill data using Google Cloud Vision.",
+      fontawesome_icon_name: "magic",
+    },
+    {
+      title: "Connect with your friends",
+      description:
+        "Add your friends from Facebook and phone contact.",
+      fontawesome_icon_name: "users",
+    },
+    {
+      title: "Easy manage and sharing",
+      description:
+        "Manage bills with cool and fun user interface.",
+      fontawesome_icon_name: "share-square",
+    },
+  ],
+  header_background             : "rgba(0, 0, 0, 0.1)",
+  topbar_title_color            : "#ffffff",
+  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
+  device_color                  : "blue",                          // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color         : "ffffff",
+  primary_text_color            : "#000",
+  content_width                 : "1170px",
+  font                          : `"Montserrat", sans-serif`,
+  link_color                    : "#A2D9F2",
+  app_title_color               : "#ffffff",
+  app_price_color               : "#ffffff",
+  app_description_color         : "#ffffff",
+  feature_title_color           : "#333333",
+  feature_text_color            : "#666666",
+  feature_icons_foreground_color: "#A2D9F2",
+  feature_icons_background_color: "#333333",
+  social_icons_foreground_color : "#666666",
+  social_icons_background_color : "#e6e6e6",
+  footer_text_color             : "#666666",
+}
